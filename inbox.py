47368dcdaaf0628684744dee8f1b78319fb5f695
ammonialime <joncairo@gmail.com>
2014-04-07 20:10:29 -0600

    Attached all email attributes to the parent so we don't poll the server each time we enter the form.

--- inbox.py
+++ inbox.py
@@ -18,7 +18,9 @@ class Inbox(npyscreen.FormBaseNew):
 
         # setup form attributes to hold message data
 
-        self.inbox_messages = self.parentApp.mail.inbox()
+        #self.inbox_messages = self.parentApp.mail.inbox()
+        self.inbox_messages = self.parentApp.mail.inbox_messages
+
         self.email_hdr_lst = []
         self.cursor_pos = 0
 
@@ -45,7 +47,7 @@ class Inbox(npyscreen.FormBaseNew):
             # get index of the currently selected email
             selected_msg_index = self.cursor_pos
             # get the uid of the mail 
-            num_msgs = len(self.inbox_messages)
+            num_msgs = len(self.parentApp.mail.inbox_messages)
             # ensure the last message is not deleted.
             if num_msgs == 1:
                 npyscreen.notify_ok_cancel("Can't delete last message in box")
@@ -53,12 +55,12 @@ class Inbox(npyscreen.FormBaseNew):
             # if we are deleting the last message show the body of the second
             # last message
                  
-            msg_uid = self.inbox_messages[selected_msg_index].uid
+            msg_uid = self.parentApp.mail.inbox_messages[selected_msg_index].uid
             # call server to delete the msg with the specified id
             rv = self.parentApp.mail.delete_msg(msg_uid)
             if rv == 'OK':
-                del self.email_hdr_lst[selected_msg_index]
-                del self.inbox_messages[selected_msg_index]
+                del self.parentApp.mail.email_hdr_lst[selected_msg_index]
+                del self.parentApp.mail.inbox_messages[selected_msg_index]
                 del self.msg_headers.msg_unread[selected_msg_index]
                 # upate the indexes of the read and unread emails.
                 self.msg_headers.update()
@@ -66,9 +68,9 @@ class Inbox(npyscreen.FormBaseNew):
                 # if we delete the last message show the one above
                 # otherwise show the one below
                 if num_msgs == selected_msg_index + 1:
-                    self.parentApp.INBOX_MSG_TXT = self.inbox_messages[selected_msg_index - 2].body
+                    self.parentApp.INBOX_MSG_TXT = self.parentApp.mail.inbox_messages[selected_msg_index - 2].body
                 else:
-                    self.parentApp.INBOX_MSG_TXT = self.inbox_messages[selected_msg_index].body
+                    self.parentApp.INBOX_MSG_TXT = self.parentApp.mail.inbox_messages[selected_msg_index].body
                 # update the display
                 self.msg_body.value = self.parentApp.INBOX_MSG_TXT 
                 self.msg_body.update()
@@ -80,11 +82,11 @@ class Inbox(npyscreen.FormBaseNew):
             # get index of the currently selected email
             selected_msg_index = self.cursor_pos
             # get the uid of the mail 
-            msg_uid = self.inbox_messages[selected_msg_index].uid
+            msg_uid = self.parentApp.mail.inbox_messages[selected_msg_index].uid
             # call server to mark as read
             rv = self.parentApp.mail.mark_read(msg_uid)
             if rv == 'OK':
-                self.inbox_messages[selected_msg_index].unread = False
+                self.parentApp.mail.inbox_messages[selected_msg_index].unread = False
                 # update the mails image so its no longer highlighted in the mail list
                 self.msg_headers.msg_unread[selected_msg_index] = False
                 self.msg_headers.update()
@@ -122,21 +124,12 @@ class Inbox(npyscreen.FormBaseNew):
 
         # get a list of email headers from/date/subject
 
-        # assemble header information for all messages in the
-        # inbox_messages list
-        self.inbox_messages.reverse()
-        for message in self.inbox_messages:
-            fmt_msg_hdr = "{0:<{width}.{width}} {1:<17.17} {2:<}".format(
-                message.sender_addr.split(' <')[0],
-                message.date, message.subject, width=20)
-            self.email_hdr_lst.append(fmt_msg_hdr)
-
         # pass the list of headers to the widget to display
-        self.msg_headers.values = self.email_hdr_lst
+        self.msg_headers.values = self.parentApp.mail.email_hdr_lst
 
         # this is the variable which contains the message value held
         # in the box below the message headers field.
-        self.msg_body.value = self.inbox_messages[0].body 
+        self.msg_body.value = self.parentApp.mail.inbox_messages[0].body 
 
     def new_mail(self, *args):
         # this gets called from the threaded mail checker process an event
@@ -144,7 +137,7 @@ class Inbox(npyscreen.FormBaseNew):
         # so before we actually can confirm we received new mail we need to check
         # whether a new piece of mail actually exists.
         new_msg = self.parentApp.mail.inbox(amount=1)[0]
-        if new_msg.uid == self.inbox_messages[0].uid:
+        if new_msg.uid == self.parentApp.mail.inbox_messages[0].uid:
             # this means we don't actually have a new message
             return
         # append the new mail to the message list
@@ -155,9 +148,9 @@ class Inbox(npyscreen.FormBaseNew):
         fmt_msg_hdr = "{0:<{width}.{width}} {1:<17.17} {2:<}".format(
             new_msg.sender_addr.split(' <')[0],
             new_msg.date, new_msg.subject, width=20)
-        self.email_hdr_lst.insert(0, fmt_msg_hdr)
-        self.msg_headers.values = self.email_hdr_lst
-        self.inbox_messages.insert(0, new_msg)
+        self.parentApp.mail.email_hdr_lst.insert(0, fmt_msg_hdr)
+        self.msg_headers.values = self.parentApp.mail.email_hdr_lst
+        self.parentApp.mail.inbox_messages.insert(0, new_msg)
         self.msg_headers.msg_unread.insert(0, True)
         # update the display
         self.msg_headers.update()
--- main_menu_login_popup.py
+++ main_menu_login_popup.py
@@ -40,10 +40,23 @@ class MainMenuPopup(npyscreen.ActionPopup):
         # notify the user that the login worked
         npyscreen.notify_confirm("Login successful", title="Success",
             form_color='STANDOUT', wrap=True, editw=1)
+        
+        # get inbox messages
+        self.parentApp.mail.inbox_messages = self.parentApp.mail.inbox()
+        self.parentApp.mail.inbox_messages.reverse()
+
+        # assemble header information for all messages in the
+        # inbox_messages list
+        self.parentApp.mail.email_hdr_lst = []
+        for message in self.parentApp.mail.inbox_messages:
+            fmt_msg_hdr = "{0:<{width}.{width}} {1:<17.17} {2:<}".format(
+                message.sender_addr.split(' <')[0],
+                message.date, message.subject, width=20)
+            self.parentApp.mail.email_hdr_lst.append(fmt_msg_hdr)
 
         # take them back to the main men 
         self.parentApp.switchForm('MAIN')
-
+        
     # this gets run when the user selects cancel on the login form.
     def on_cancel(self):
         self.parentApp.switchFormPrevious()
